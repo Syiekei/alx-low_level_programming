3dc75087a291dcf007bff31bf17b2de52fd0bd6c
Syiekei <otworiwycliff88@gmail.com>
2023-02-16 20:24:45 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types
- * on the computer it is compiled and run on..
+ * Prints the size of various types based on
+ * the computer it is compiled and run on..
  * Return: Always 0
  */
 
