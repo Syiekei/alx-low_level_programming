41c9fda7a7866a3f1d70473df6e7fac614069b05
Syiekei <otworiwycliff88@gmail.com>
2023-02-21 06:12:22 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-  print_alphabet();
-  return (0);
+	int r;
+	{
+		r = _islower('H');
+		_putchar(r + '0');
+		r = _islower('o');
+		_putchar(r + '0');
+		r = _islower(108);
+		_putchar(r + '0');
+		_putchar('\n');
+		return (0);
+	}
 }
