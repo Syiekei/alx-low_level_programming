0de8f829bcc8ae8946a2a4bc203856a24186ccaf
Syiekei <otworiwycliff88@gmail.com>
2023-03-11 05:45:08 -0800

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,5 @@
-#include "main.h"
+#include "main.h")
 #include<stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,16 +7,6 @@
  */
 int main(void)
 {
-	int i = 0;
-while (str[i])
-{
-_putchar(str[i]);
-i++;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-_putchar('\n');
-}
-puts("\"Programming is like building a multilingual puzzle");
-{
-return (0);
-}
-
